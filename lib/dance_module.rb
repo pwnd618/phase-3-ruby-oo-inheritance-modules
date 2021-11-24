517bfc7a1e1810b9bd8b3c720784678905e3b8bc
webster <wbian618@gmail.com>
2021-11-24 12:59:03 -0500

    commit tree d1d219185a6661877958003a64e90d47122f526b

---
+++ lib/dance_module.rb
@@ -0,0 +1,17 @@
+moduel Dance
+    def twirl
+        "I'm twirling!"
+    end
+
+    def jump
+        "Look how high I'm jumping!"
+    end
+
+    def pirouette
+        "Im doing a pirouette"
+    end
+
+    def take_a_bow
+        "Thank you, thank you. It was a please to dance for you all."
+    end
+end
---
+++ lib/meta_dancing_module.rb
@@ -0,0 +1,5 @@
+module MetaDancing
+    def metadata
+        "This class produces objets that love to dance"
+    end
+end
